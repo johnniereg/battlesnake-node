1ad40a3dad0b09ffd33f9b3a669c75f97122fddb
johnniereg <johnniereg@gmail.com>
2018-02-27 19:27:01 -0800

    Wrote a basic logic check to move up then right

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const logger = require('morgan')
-const app = express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
   poweredByHandler
-} = require('./handlers.js')
+} = require('./handlers.js');
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001))
+app.set('port', (process.env.PORT || 9001));
 
-app.enable('verbose errors')
+app.enable('verbose errors');
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(poweredByHandler)
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(poweredByHandler);
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
@@ -30,30 +30,43 @@ app.post('/start', (request, response) => {
     color: '#DFFF00',
     head_url: 'http://www.placecage.com/c/200/200', // optional, but encouraged!
     taunt: "Let's do thisss thang!", // optional, but encouraged!
-  }
+  };
 
-  return response.json(data)
-})
+  return response.json(data);
+});
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
+  console.log(request);
+  console.log(response);
   // NOTE: Do something here to generate your move
 
+  const counter = 0;
   // Response data
-  const data = {
-    move: 'up', // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+
+  if (counter % 2 === 0) {
+    const data = {
+      move: 'up', // one of: ['up','down','left','right']
+      taunt: 'Outta my way, snake!', // optional, but encouraged!
+    };
+  } else {
+    const data = {
+      move: 'right', // one of: ['up','down','left','right']
+      taunt: 'Outta my way, snake!', // optional, but encouraged!
+    };
   }
 
-  return response.json(data)
-})
+  counter += 1;
+
+  return response.json(data);
+});
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
-app.use('*', fallbackHandler)
-app.use(notFoundHandler)
-app.use(genericErrorHandler)
+app.use('*', fallbackHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
 
 app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'))
-})
+  console.log('Server listening on port %s', app.get('port'));
+});
